502a3121d41e5a2e2707a0a436e5161985ade327
bcteenager <48722094+bcteenager@users.noreply.github.com>
2020-08-05 22:14:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  document.getElementById('main-content').innerHTML = 'Hello, github :)'
+  document.getElementById('main-content').innerHTML = 'I am Jiang :)'
 }
